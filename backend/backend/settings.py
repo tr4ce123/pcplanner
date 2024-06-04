f83851f8e7d6a96ad58d59fe0a3a8d90e68e9a23
Gregory Glasby <gglasby@unc.edu>
2024-06-03 21:02:18 -0400

    env vars

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -134,11 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-
-STATICFILES_DIRS = [
-    BASE_DIR / 'backend' / 'staticfiles',
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 
